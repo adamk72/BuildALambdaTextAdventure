f1c7b633b45f9fb46cc0d2aa172532c462f5816c
Adam Kecskes <adam@kecskes.net>
2024-10-27 19:20:18 -0500

    add direction looking

--- src/Repl/Repl.hs
+++ src/Repl/Repl.hs
@@ -23,6 +23,9 @@ read_ = TIO.putStr replPrompt >>
         hFlush stdout >>
         TIO.getLine
 
+print_ :: T.Text -> IO ()
+print_ = TIO.putStrLn
+
 eval_ :: T.Text -> State Player T.Text
 eval_ input = do
   player <- get
@@ -34,11 +37,22 @@ eval_ input = do
     "look around" -> do
         let text = executeLook player (Just "around")
         return text
-    -- "look " <> direction | direction `elem` ["north", "south", "east", "west"] -> do
-    --     let text = executeLook player (Just direction)
-    --     return text
-    _ ->
-        return $ "I don't know how to '" <> input <> "'."
+    other -> case isDirectionalLook other of
+        Just direction -> do
+            let text = executeLook player (Just direction)
+            return text
+        Nothing ->
+            return $ "I don't know how to '" <> input <> "'."
 
-print_ :: T.Text -> IO ()
-print_ = TIO.putStrLn
+-- Helper function to handle directional looking
+isDirectionalLook :: T.Text -> Maybe T.Text
+isDirectionalLook input =
+    let directions = ["north", "south", "east", "west"]
+        prefix = "look "
+    -- isPrefixOf: takes two Texts and returns True if and only if the first is a prefix of the second.
+    in if T.isPrefixOf prefix input
+       then let direction = T.drop (T.length prefix) input
+            in if direction `elem` directions
+               then Just direction
+               else Nothing
+       else Nothing
