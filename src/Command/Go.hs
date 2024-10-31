593fb8899ab7be4b74669e7681ad003dd60d40e9
Adam Kecskes <adam@kecskes.net>
2024-10-31 18:14:44 -0500

    update executeDoc to passing

--- src/Command/Go.hs
+++ src/Command/Go.hs
@@ -4,22 +4,23 @@ import           Control.Monad.State
 import           Core.State          (Character (..), GameWorld (..),
                                       Location (..))
 import           Data.Text           (Text)
-
-validLocs :: [Text]
-validLocs = ["cave", "meadow"]
+import Data.List (find)
 
 executeGo :: Maybe Text -> State GameWorld Text
 executeGo gotoLoc = do
   gw <- get
   let ac = activeCharacter gw
+      validLocs = locations gw
   case gotoLoc of
-    Just known | known `elem` validLocs  ->
-      if locTag (currentLocation ac) == known
-      then return $ "You're already in " <> known <> "."
+    Just targetLocTag | targetLocTag `elem` map locTag validLocs  ->
+      if locTag (currentLocation ac) == targetLocTag
+      then return $ "You're already in " <> targetLocTag <> "."
       else do
-        let newLoc = Location { locTag = known, locName = "test"}
-        let newAc = ac { currentLocation = newLoc }
-        put gw { activeCharacter = newAc  }
-        return $ "Moving to " <> known <> "."
+        case find (\loc -> locTag loc == targetLocTag) validLocs of
+          Just newLoc -> do
+            let newAc = ac { currentLocation = newLoc }
+            put gw { activeCharacter = newAc  }
+            return $ "Moving to " <> targetLocTag <> "."
+          Nothing -> return "Unable to find that location in this world."
     Just l -> return $ "Unknown location: " <> l <> "."
     Nothing -> return "Unable to find a location at all."
