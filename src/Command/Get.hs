dd1fc6bf0e3d615294d1e89fa568d2f9d4fbccb9
Adam Kecskes <adam@kecskes.net>
2024-11-10 20:43:18 -0600

    refactor getItem

--- src/Command/Get.hs
+++ src/Command/Get.hs
@@ -2,49 +2,43 @@ module Command.Get (module Command.Get) where
 
 import           Command.CommandExecutor
 import           Core.GameMonad
-
 import           Core.Message
 import           Core.State
 import           Data.Text               (Text)
 import           Parser.Types
 import           Parser.Utils
 
+-- | Attempt to take an item from a specific location or container
 getItem :: Text -> Maybe Text -> [Item] -> Actor -> GameWorld -> GameStateText
-getItem itemTag maybeContTag visibleItems actor gw
-    | tagInItemList itemTag visibleItems =
-        case findItemByTag itemTag gw of
-            Just validItem -> do
-                let objLoc = getLocation validItem
-                    acInv = getActorInventory gw
-                if objLoc == acInv -- Todo: also check if inside of a container in the actor's inventory
-                then return $ "You already have " <> getName validItem
-                else do
-                    case maybeContTag of
-                        Just containerOrLoc ->
-                            case findAnyLocationByTag containerOrLoc gw of
-                                Just locFound -> do
-                                    if itemExistsAtLoc itemTag locFound gw True
-                                    then do
-                                        let updatedGW = moveItemLoc validItem acInv gw
-                                        modifyGameWorld (const updatedGW)
-                                        msg $ PickedUp itemTag (getName actor)
-                                    else return $ "That can't be found in " <> containerOrLoc
-                                Nothing -> do
-                                    case findItemByTag containerOrLoc gw of
-                                        Just containerFound -> do
-                                            if isContainer containerFound
-                                            then do
-                                                let updatedGW = moveItemLoc validItem acInv gw
-                                                modifyGameWorld (const updatedGW)
-                                                msg $ PickedUp itemTag (getName actor)
-                                            else return $ containerOrLoc <> " isn't a container."
-                                        Nothing -> msg $ InvalidItem containerOrLoc
-                        Nothing -> do
-                            let updatedGW = moveItemLoc validItem acInv gw
-                            modifyGameWorld (const updatedGW)
-                            msg $ PickedUp itemTag (getName actor)
-            Nothing -> msgGameWordError $ ItemDoesNotExist itemTag
-    | otherwise = msg $ InvalidItem itemTag
+getItem itemTag srcM visibleItems actor gw
+    | not (tagInItemList itemTag visibleItems) = msg $ InvalidItem itemTag
+    | otherwise = case findItemByTag itemTag gw of
+        Nothing -> msgGameWordError $ ItemDoesNotExist itemTag
+        Just item -> do
+            let acInv = getActorInventory gw
+            if getLocation item == acInv
+            then return $ "You already have " <> getName item
+            else tryGetItem item acInv srcM
+
+  where
+    tryGetItem :: Item -> Location -> Maybe Text -> GameStateText
+    tryGetItem item locOrItem = \case
+        Nothing -> moveAndMsg item locOrItem
+        Just src -> case findAnyLocationByTag src gw of
+            Just loc -> if itemExistsAtLoc itemTag loc gw True
+                       then moveAndMsg item locOrItem
+                       else return $ "That can't be found in " <> src
+            Nothing -> case findItemByTag src gw of
+                Just container -> if isContainer container
+                                then moveAndMsg item locOrItem
+                                else return $ src <> " isn't a container."
+                Nothing -> msg $ InvalidItem src
+
+    moveAndMsg :: Item -> Location -> GameStateText
+    moveAndMsg item acInv = do
+        let updatedGW = moveItemLoc item acInv gw
+        modifyGameWorld (const updatedGW)
+        msg $ PickedUp itemTag (getName actor)
 
 executeGet :: CommandExecutor
 executeGet expr = do
@@ -53,15 +47,12 @@ executeGet expr = do
         acLoc = getActiveActorLoc gw
         visibleItems = getItemsAtLocDeep acLoc gw True
         handle = \case
-            AtomicExpression {} ->
-                msg GetWhat
+            AtomicExpression {} -> msg GetWhat
             UnaryExpression _ (NounClause itemTag) ->
                 getItem itemTag Nothing visibleItems ac gw
-            BinaryExpression {} ->
-                msg GetWhat
-            ComplexExpression _ (NounClause itemTag) (PrepClause prep) (NounClause containerTag)
-                | prep `isPrepVariantOf` "from" ||  prep `isPrepVariantOf` "in" ->
-                    getItem itemTag (Just containerTag) visibleItems ac gw
-            ComplexExpression {} ->
-                return "TBD"
+            BinaryExpression {} -> msg GetWhat
+            ComplexExpression _ (NounClause itemTag) (PrepClause prep) (NounClause src)
+                | prep `isPrepVariantOf` "from" || prep `isPrepVariantOf` "in" ->
+                    getItem itemTag (Just src) visibleItems ac gw
+            ComplexExpression {} -> return "TBD"
     handle expr
