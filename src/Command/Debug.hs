580a58afb7fd9203ca0a61060fd1ef4489af8578
Adam Kecskes <adam@kecskes.net>
2024-12-13 17:05:30 -0600

    update feedback from malformed debug command

--- src/Command/Debug.hs
+++ src/Command/Debug.hs
@@ -6,6 +6,7 @@ import           Command.Message
 import           Command.Message.Debug
 import           Core.GameMonad
 import           Parser.Types
+import           System.FilePath         ((</>))
 import           Text.RawString.QQ
 
 executeDebug :: CommandExecutor
@@ -13,7 +14,7 @@ executeDebug expr = do
     gw <- getWorld
     case expr of
         UnaryCmdExpression _ (NounClause "world") -> msg $ ShowWorldState $ formatWorld gw
-        ComplexCmdExpression _ (NounClause "replay") _ (NounClause filename) -> undefined
+        ComplexCmdExpression _ (NounClause "replay") _ (NounClause filename) -> executeReplay $ "logs" </> filename
         _ -> msg $ InvalidDebugCommand [r|Valid debug commands are \":debug\" (or \":dbg\" or \":d\") followed by
     world                  -- gives current world information
     replay with <filename> -- replays file of log/<filename>|]
