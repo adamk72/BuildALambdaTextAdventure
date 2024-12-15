d0dd2441b0763bc64088259c9afaabcfb3613b9a
Adam Kecskes <adam@kecskes.net>
2024-12-14 20:53:12 -0600

    wip: update main to better support features of 'options.applicative'

--- app/CmdOptions.hs
+++ app/CmdOptions.hs
@@ -1,43 +1,98 @@
-module CmdOptions (parse, AdventureOptions(AdventureOptions), showHelp) where
+module CmdOptions (AdventureOptions (..), RunCommand (..), findAdventurePath, getParserInfo, options, programDesc) where
 
-import           Options.Applicative (ParseError (ShowHelpText), Parser,
-                                      ParserInfo, defaultPrefs, execParser,
-                                      fullDesc, header, help, helper, info,
-                                      infoOption, long, metavar, parserFailure,
-                                      progDesc, progDescDoc, renderFailure,
-                                      short, strOption, (<**>))
-import           Prettyprinter       (Pretty (pretty), indent, vsep)
+import           Core.State.JSONTypes
+import           Data.Text            as T (Text, intercalate, pack, unpack)
+import           Options.Applicative
+import           Prettyprinter        (Pretty (pretty), indent, vsep)
 
-newtype AdventureOptions = AdventureOptions String
+newtype AdventureOptions = AdventureOptions { optCommand :: RunCommand }
 
-choice :: Parser AdventureOptions
-choice = AdventureOptions
-      <$> strOption
-          ( long "adventure"
-          <> short 'a'
-          <> metavar "NAME"
-          <> help "Name of adventure to load." )
+data RunCommand
+    = Run Text
+    | Replay Text
+    | ReplayFile Text FilePath
+    deriving (Show)
 
-versionOption :: String ->  Parser (a -> a)
-versionOption s = infoOption s (long "version" <> help "Show version")
+options :: [(FilePath, Either String Metadata)] -> Parser AdventureOptions
+options adventures = AdventureOptions <$> commandParser adventures
 
-parse :: String -> IO AdventureOptions
-parse = execParser . opts
-
--- Todo: Examine this suggestion from Claude a little more closely.
--- I don't like how it seems to subvert the normal path; there may be a better choice by
--- following a more idiomatic use of Options.Applicative inside of Main.hs.
-showHelp :: String -> IO ()
-showHelp s = do
-    let parser = opts s
-    let failure = parserFailure defaultPrefs parser (ShowHelpText Nothing) []
-    let (help_txt, _exit_code) = renderFailure failure "TextAdventure-exe"
-    putStr help_txt
-
-opts :: String -> ParserInfo AdventureOptions
-opts s = info (choice <**> versionOption s <**> helper)
-    ( fullDesc
-    <> progDesc "Run the named text adventure."
-    <> header "Haskell Adventure - a journey into fun!"
-    <> (progDescDoc . Just $ indent 2 $ vsep [pretty s])
+commandParser :: [(FilePath, Either String Metadata)] -> Parser RunCommand
+commandParser adventures = subparser
+    ( command "run" (info (runOptions adventures) (progDesc "Run an adventure"))
+    <> command "replay" (info (replayOptions adventures) (progDesc "Replay an adventure"))
+    <> command "replay-file" (info replayFileOptions (progDesc "Replay an adventure using a specific file"))
     )
+
+runOptions :: [(FilePath, Either String Metadata)] -> Parser RunCommand
+runOptions adventures = Run <$> adventureOption adventures
+
+replayOptions :: [(FilePath, Either String Metadata)] -> Parser RunCommand
+replayOptions adventures = Replay <$> adventureOption adventures
+
+replayFileOptions :: Parser RunCommand
+replayFileOptions = ReplayFile
+    <$> strOption
+        ( long "adventure"
+        <> short 'a'
+        <> metavar "NAME"
+        <> help "Name of the adventure to replay"
+        )
+    <*> strOption
+        ( long "file"
+        <> short 'f'
+        <> metavar "FILEPATH"
+        <> help "File containing commands to replay"
+        )
+
+adventureOption :: [(FilePath, Either String Metadata)] -> Parser Text
+adventureOption adventures = strOption
+    ( long "name"
+    <> short 'n'
+    <> metavar "NAME"
+    <> help (unpack $ "Name of the adventure. Available: " <> intercalate ", " (map getAdventureName adventures))
+    )
+
+getAdventureName :: (FilePath, Either String Metadata) -> Text
+getAdventureName (_, Right md) = launchTag md
+getAdventureName (path, _)     = pack path
+
+findAdventurePath :: Text -> [(FilePath, Either String Metadata)] -> Maybe FilePath
+findAdventurePath tag = foldr check Nothing
+  where
+    check (path, Right md) acc = if launchTag md == tag then Just path else acc
+    check _ acc                = acc
+
+programDesc :: [(FilePath, Either String Metadata)] -> String
+programDesc adventures = unlines
+    [ "Haskell Text Adventure Game"
+    , "Available adventures:"
+    ] ++ concatMap showAdventure adventures
+  where
+    showAdventure (_, Right md) = unpack $ "  " <> title md <> " (" <> launchTag md <> ") - " <> description md <> "\n"
+    showAdventure (path, _)     = unpack $ "  " <> pack path <> " (invalid metadata)\n"
+
+versionOption :: String -> Parser (a -> a)
+versionOption ver = infoOption ver
+    ( long "version"
+    <> short 'v'
+    <> help "Show version information"
+    )
+
+getParserInfo :: [(FilePath, Either String Metadata)] -> String -> ParserInfo AdventureOptions
+getParserInfo adventures ver = info (helper <*> versionOption ver <*> options adventures)
+    ( fullDesc
+    <> progDesc "Run a text adventure game."
+    <> header "Haskell Adventure - a journey into fun!"
+    <> (progDescDoc . Just $ indent 2 $ vsep [pretty (formatAdventures adventures)])
+    )
+  where
+    formatAdventures :: [(FilePath, Either String Metadata)] -> String
+    formatAdventures advs = unlines $ "Available adventures:" : map formatAdventure advs
+
+    formatAdventure :: (FilePath, Either String Metadata) -> String
+    formatAdventure (_, Right md) = unpack $
+        "  " <> title md <> " (v" <> version md <> ") by " <> author md <>
+        "\n    " <> description md <>
+        "\n    Launch with: --name " <> launchTag md
+    formatAdventure (path, Left _) =
+        "  Invalid adventure at: " ++ path
--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,16 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Main (main) where
 
-import           CmdOptions           as Cmd (parse, showHelp)
-import           Control.Monad        (forM_, void)
+import           CmdOptions
+import           Control.Monad        (forM_)
 import           Core.Launch          (launch)
 import           Core.State.JSONTypes (Metadata (..))
 import           Data.Either          (isRight)
-import           Data.List            (find)
-import           Data.Text            (Text, intercalate, pack, unpack)
+import           Data.Text
 import qualified Data.Text.IO         as TIO
 import           JsonProcessing       (getJsonFilePaths, readAllMetadata, storyDirectory)
-import           System.Environment   (getArgs)
-import           System.Exit          (ExitCode (ExitFailure), exitSuccess, exitWith)
-
-data Command
-    = RunAdventure FilePath
-    | ReplayAdventure FilePath
-    | ShowHelp
-    | InvalidAdventure Text
-
-parseArgs :: [(FilePath, Either String Metadata)] -> [String] -> Command
-parseArgs adventures = \case
-    ["-a", option] -> findAdventureCommand RunAdventure option
-    ["-r", option] -> findAdventureCommand ReplayAdventure option
-    _ -> ShowHelp
-  where
-    findAdventureCommand constructor option =
-        let tag = pack option
-        in case findAdventureByTag tag adventures of
-            Just path -> constructor path
-            Nothing   -> InvalidAdventure tag
-
-    findAdventureByTag tag = fmap fst . find (\(_, result) -> either (const False) ((== tag) . launchTag) result)
+import           Options.Applicative
+import           Prelude              as P
+import           System.Exit          (exitFailure, exitSuccess)
 
 formatMetadata :: Metadata -> Text
 formatMetadata md = title md <> " (" <> launchTag md <> ") - " <> description md
@@ -47,29 +25,29 @@ main = do
             Left err -> TIO.putStrLn $ "Error loading " <> pack path <> ": " <> pack err
             Right _  -> return ()
 
-    let validAdventures = filter (isRight . snd) adventures
-        adventureDescriptions = map (formatMetadata . either (error "No valid adventures found.") id . snd) validAdventures
+    let version = "0.1.0.0" -- Todo: centralize this later
+    whichCommand <- execParser $ getParserInfo adventures version
+    case whichCommand of
+        AdventureOptions cmd -> runCommand adventures cmd
 
-    getArgs >>= \args ->
-        case parseArgs validAdventures args of
-            RunAdventure path -> runGameWithOption path False
-            ReplayAdventure path -> runGameWithOption path True
-            InvalidAdventure name -> do
-                TIO.putStrLn $ "Invalid adventure name: " <> name <> "\n"
-                Cmd.showHelp (unpack $ intercalate "\n" adventureDescriptions)
-            ShowHelp ->
-                displayHelp adventureDescriptions
+runCommand :: [(FilePath, Either String Metadata)] -> RunCommand -> IO ()
+runCommand adventures cmd = case cmd of
+    Run name          -> runGame adventures name False
+    Replay name       -> runGame adventures name True
+    ReplayFile name _ -> runGame adventures name True  -- Todo: Handle custom file path
 
-displayHelp :: [Text] -> IO ()
-displayHelp = void . Cmd.parse . unpack . intercalate "\n"
-
-runGameWithOption :: FilePath -> Bool -> IO ()
-runGameWithOption option replayMode = do
-   result <- launch option replayMode
-   case result of
-        Left msg -> do
-            TIO.putStrLn $ "Game ended with message: " <> msg
-            exitWith (ExitFailure 1)
-        Right () -> do
-            TIO.putStrLn "Thanks for playing!"
-            exitSuccess
+runGame :: [(FilePath, Either String Metadata)] -> Text -> Bool -> IO ()
+runGame adventures name replayMode =
+    case findAdventurePath name adventures of
+        Nothing -> do
+            TIO.putStrLn $ "Invalid adventure name: " <> name
+            exitFailure
+        Just path -> do
+            result <- launch path replayMode
+            case result of
+                Left msg -> do
+                    TIO.putStrLn $ "Game ended with error: " <> msg
+                    exitFailure
+                Right () -> do
+                    TIO.putStrLn "Thanks for playing!"
+                    exitSuccess
